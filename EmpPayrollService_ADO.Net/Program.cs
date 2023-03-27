2f799c1a7cec17142eaa221e922b0e1e7d17d8db
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-27 11:33:49 +0530

    Added to Employee Payroll Service Using ADO.Net

---
+++ EmpPayrollService_ADO.Net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpPayrollService_ADO.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee payroll service using ADO.Net");
+        }
+    }
+}
